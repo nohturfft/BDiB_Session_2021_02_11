90e325591bb131059240394e6eca94644255fb33
nohturfft <nohturfft@gmail.com>
2021-02-10 11:47:15 +0000

    Adding packages

--- packages.R
+++ packages.R
@@ -2,3 +2,6 @@ if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("rmarkdown")) install.packages("rmarkdown")
 if (!require("knitr")) install.packages("knitr")
 if (!require("DT")) install.packages("DT")
+if (!require("htmlwidgets")) install.packages("htmlwidgets")
+if (!require("plotly")) install.packages("plotly")
+
