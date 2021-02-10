c328b28e396bed4cb0e8baa411fc940e45a1c684
nohturfft <nohturfft@gmail.com>
2021-02-10 10:40:51 +0000

    First commit

---
+++ packages.R
@@ -0,0 +1,4 @@
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("rmarkdown")) install.packages("rmarkdown")
+if (!require("knitr")) install.packages("knitr")
+if (!require("DT")) install.packages("DT")
